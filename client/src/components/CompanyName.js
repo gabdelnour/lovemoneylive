8ab2e973b725cd3b59a0f1fb904d5db9ca9ae8b3
Gustavo Abdelnour <gurabdelnour@gmail.com>
2019-10-27 13:46:59 -0400

    Cleaned up API ENDPOINTS

---
+++ client/src/components/CompanyName.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CompanyName = props => (
+    <p>{props.companyName}</p>
+)
+
+export default CompanyName;
--- client/src/pages/CompanyDescription.js
+++ client/src/pages/CompanyDescription.js
@@ -1,26 +1,48 @@
 import React from 'react'
+import axios from 'axios';
+import CompanyName from '../components/CompanyName';
 
 
 class CompanyDescription extends React.Component {
 state = {
     companies: [],
+    ticker: '',
+    companyProfile: {},
+    balanceSheet: {},
+    incomeStatement: {}, 
+    enterpriseValue: {},
 }
 
-    handleSearch = event => {
-            fetch(`/api/v3/company/profile/${event.target.value}`)
-            .then(response => response.json())
-            .then(data => console.log(data))
-    }
+handleInputChange = event => this.setState({ ticker: event.target.value })
+
+getCompanyProfile = async event => {
+    event.preventDefault();
+    const { data } = await axios.get(`/companyBundle/${this.state.ticker}`)
+    const { companyProfile, balanceSheet, incomeStatement, enterpriseValue } = data;
+    this.setState({ companyProfile, balanceSheet, incomeStatement, enterpriseValue })
+}
 
     render(){
+        const { companyProfile } = this.state
+        console.log(this.state);
         return(
             <div>
-            <h1>Search for a company</h1>
-            <input 
-                type="Text"
-                placeholder="Company Name"
-                onChange={this.handleSearch}
-            />
+                <h1>Search for a company</h1>
+                <form onSubmit={this.getCompanyProfile}>
+                    <input 
+                        type="Text"
+                        placeholder="Company Name"
+                        onChange={this.handleInputChange}
+                    />
+                    <input 
+                        type="submit"
+                        value="Get Company Bundle"
+                    />
+                </form>
+                <div>
+                    <h1>Company Name</h1>
+                    {companyProfile.profile && <CompanyName companyName={companyProfile.profile.companyName} />}
+                </div>
             </div>
 
         )
