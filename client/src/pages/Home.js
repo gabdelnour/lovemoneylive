4c5cc696ae6410c9552e68f7130871aaec166765
NDQM88 <Nicholas.D.Quintero@gmail.com>
2019-10-27 01:53:36 -0400

    Added some CSS

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
-const Home = () => (
-  <div className="home">
-    <h1>This is the Home Page</h1>
-    <h2>I would make it look awesome but we have bigger fish to fry...</h2>
-  </div>
-)
 
-export default Home
+class Home extends React.Component {
+  state = {
+      companies: [],
+  }
+  
+      handleSearch = event => {
+              fetch(`/api/v3/company/profile/${event.target.value}`)
+              .then(response => response.json())
+              .then(data => console.log(data))
+      }
+  
+      render(){
+          return(
+              <div className="home">
+              <h1>Welcome to Love$Life<br/>
+              {new Date().toLocaleTimeString()}.</h1>
+              <input 
+                  className="wynput"
+                  type="Text"
+                  name="name"
+                  autoComplete="off"
+                  placeholder="Ticker Symbol"
+                  onChange={this.handleSearch}
+              />
+              </div>
+          )
+      }
+  }
+
+  export default Home
