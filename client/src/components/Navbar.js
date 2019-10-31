5af0c3f81a259b0b964773f44e4cb2361a2d81d3
cristhianFG93 <cris.f.g93@hotmail.com>
2019-10-30 21:34:00 -0400

    enhancement-#56-navBAR-THE-END

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import '../cssFiles/navBar.css'
  
 const Navbar = () => (
-  <nav className="nav">
-    <ul className="ul">
-      <li className="list"><NavLink exact to="/">Home</NavLink></li>
-      <li className="list"><NavLink exact to="/CompanyDescription">Company Description</NavLink></li>
-      <li className="list"><NavLink exact to="/CompanyList">Stock Company List</NavLink></li>
+  <nav className="menu">
+    <ul className="main">
+      <li className="homelogo"><NavLink exact to="/">Home</NavLink></li>
+      <li className="descriptionlogo"><NavLink exact to="/CompanyDescription">Company Description</NavLink></li>
+      <li className="Companylistlogo"><NavLink exact to="/CompanyList">Stock Company List</NavLink></li>
     </ul>
   </nav>
 )
+
  
 export default Navbar
- 
